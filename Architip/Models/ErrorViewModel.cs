65db24219e7b83cae3744a85d72f00cc250ab0c9
parthasarathy tamilselvam <sarathy827@hotmail.com>
2020-10-10 22:41:15 +0100

    Initial Checkin

---
+++ Architip/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Architip.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
